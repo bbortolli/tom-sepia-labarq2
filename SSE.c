4301f2a3fa5bcb89badb81d28a340514f90561d9
Ricardo1701 <ricardo_1701@hotmail.com>
2014-12-09 17:35:04 -0200

    Update SSE.c
    Conversão de inteiro para float em Assembly sequencial, e processa 6 bytes de cor a cada iteração do For

--- SSE.c
+++ SSE.c
@@ -15,14 +15,14 @@ int main(int argc, char** argv)
 	clock_t start, end;
 	double cpu_time_used;
 	char formato[5], info[24];
-	unsigned char * imagem, *temp, soma[4] = { 40, 20, 0, 0 }, subtracao[4] = { 0, 0, 20, 0 };
+	unsigned char * imagem, *temp, soma[8] = { 40, 20, 0, 40, 20, 0, 0, 0 }, subtracao[8] = { 0, 0, 20, 0, 0, 20, 0, 0 };
 	int media, redt, red, green, blue, j, *temp2, *temp3, * temp4;
 	FILE *file;
 	long int tamanhoFile, tamanhoImg;
 	char inputtFileDir[64] = "in\\", outputFileDir[64] = "out\\";
-	struct pontoflutuante a, b;
+	struct pontoflutuante fatores, b, c;
 
-	a.x = 0.3; a.y = 0.59; a.z = 0.11; a.w = 1.0; 
+	fatores.x = 0.3; fatores.y = 0.59; fatores.z = 0.11; fatores.w = 1.0;
 
 
 
@@ -58,7 +58,7 @@ int main(int argc, char** argv)
 	
 	
 
-		for (j = 0; j < tamanhoImg - 1; j += 3)
+		for (j = 0; j < tamanhoImg - 1; j += 6)
 		{
 			// media = 0.3*r + 0.59*g + 0.11*b
 			// r = media + 40
@@ -67,50 +67,94 @@ int main(int argc, char** argv)
 
 			
 			temp = &imagem[j];
-			//red = (int)imagem[j];
-			///green = (int)imagem[j + 1];
-			//blue = (int)imagem[j + 2];
-			//redt = (int)imagem[j + 3];
-
-
-			b.x = (float)imagem[j];
-			b.y = (float)imagem[j + 1];
-			b.z = (float)imagem[j + 2];
-			b.w = (float)imagem[j + 3];
-
+						
 
 			__asm
 			{
-				lea eax, b
-				lea ebx, a
-				movdqu xmm0, [eax]
-				movdqu xmm1, [ebx]
-				mulps xmm0, xmm1
-				movdqu [eax], xmm0
+				// Converte para floats 8 bytes da imagem e e coloca em b e c
+				finit
+				mov ebx, temp
+				mov al, [ebx]
+				mov red, eax
+				fild red
+				fstp b
+				mov al, [ebx + 1]
+				mov green, eax
+				fild green
+				fstp b[4]
+				mov al, [ebx + 2]
+				mov blue, eax
+				fild blue
+				fstp b[8]
+				mov al, [ebx + 3]
+				mov redt, eax
+				fild redt
+				fstp b[12]
+
+				mov al, [ebx + 3 ] 
+				mov red, eax
+				fild red
+				fstp c
+				mov al, [ebx + 4]
+				mov green, eax
+				fild green
+				fstp c[4]
+				mov al, [ebx + 5]
+				mov blue, eax
+				fild blue
+				fstp c[8]
+				mov al, [ebx + 6]
+				mov redt, eax
+				fild redt
+				fstp c[12]
+
+				
+				// Multiplica as cores das imagems pelos fatores
+				lea eax, fatores
+				lea ebx, b
+				lea ecx, c
+				movups xmm0, [eax]
+				movups xmm1, [ebx]
+				movups xmm2, [ecx]
+				mulps xmm1, xmm0
+				mulps xmm2, xmm0
+				movups [ebx], xmm1
+				movups [ecx], xmm2
+
 			}
 
-			imagem[j] = (unsigned char)b.x;
+
+
+			imagem[j]	    = (unsigned char)b.x;
 			imagem[j + 1] = (unsigned char)b.y;
 			imagem[j + 2] = (unsigned char)b.z;
 			imagem[j + 3] = (unsigned char)b.w;
 
+			imagem[j + 3] = (unsigned char)c.x;
+			imagem[j + 4] = (unsigned char)c.y;
+			imagem[j + 5] = (unsigned char)c.z;
+			imagem[j + 6] = (unsigned char)c.w;
 
 			imagem[j] += imagem[j + 1] + imagem[j + 2];
 			imagem[j + 1] = imagem[j];
 			imagem[j + 2] = imagem[j];
 
+			imagem[j + 3] += imagem[j + 4] + imagem[j + 5];
+			imagem[j + 4] = imagem[j + 3];
+			imagem[j + 5] = imagem[j + 3];
+
 			__asm
 			{
 				mov eax, temp
-				movd mm0, [eax]
-				movd mm3, soma
-				movd mm4, subtracao
+				movq mm0, [eax]
+				movq mm3, soma
+				movq mm4, subtracao
 				paddusb mm0, mm3
 				psubusb mm0, mm4
 
 
 				mov eax, temp
-				movd[eax], mm0
+				movq[eax], mm0
 
 				emms
 			}
