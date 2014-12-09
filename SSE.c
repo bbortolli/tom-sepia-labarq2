a58734941b16d0231ea6bf80730a997fe1f8fee7
Ricardo1701 <ricardo_1701@hotmail.com>
2014-12-09 15:36:36 -0200

    Update SSE.c
    conversão entre byte a float ainda em C

--- SSE.c
+++ SSE.c
@@ -3,7 +3,7 @@
 #include <xmmintrin.h>
 
 // referencia: [1]
-struct fatores
+struct pontoflutuante
 {
 	float x, y, z, w;
 
@@ -15,13 +15,12 @@ int main(int argc, char** argv)
 	clock_t start, end;
 	double cpu_time_used;
 	char formato[5], info[24];
-	unsigned char * imagem, *temp;
-	//float fatores[4] = { 0.3, 0.59, 0.11, 1.0 };
-	int media, red, green, blue, j, *temp2, *temp3, soma[4] = { 40, 20, 0, 0 }, subtracao[4] = { 0, 0, 20, 0 },* temp4;
+	unsigned char * imagem, *temp, soma[4] = { 40, 20, 0, 0 }, subtracao[4] = { 0, 0, 20, 0 };
+	int media, redt, red, green, blue, j, *temp2, *temp3, * temp4;
 	FILE *file;
 	long int tamanhoFile, tamanhoImg;
 	char inputtFileDir[64] = "in\\", outputFileDir[64] = "out\\";
-	struct fatores a;
+	struct pontoflutuante a, b;
 
 	a.x = 0.3; a.y = 0.59; a.z = 0.11; a.w = 1.0; 
 
@@ -55,159 +54,73 @@ int main(int argc, char** argv)
 	start = clock();
 
 
-	//Calculos para o tom de sepia sequencial
-	/*
-	for (j = 0; j < tamanhoImg - 1; j += 3)
-	{
-		// media = 0.3*r + 0.59*g + 0.11*b
-		// r = media + 40
-		// g = media + 20
-		// b = media - 20
-
-		red = (int)imagem[j];
-		green = (int)imagem[j + 1];
-		blue = (int)imagem[j + 2];
-		__asm
-		{
-			// obtendo 0.3*red
-			mov eax, red
-				mov ebx, 3
-				mul ebx
-				mov ebx, 10
-				mov edx, 0
-				div ebx
-				mov red, eax
-
-				// obtendo 0.59*g
-				mov eax, green
-				mov ebx, 59
-				mul ebx
-				mov ebx, 100
-				mov edx, 0
-				div ebx
-				mov green, eax
-
-				// obtendo 0.11*b
-				mov eax, blue
-				mov ebx, 11
-				mul ebx
-				mov ebx, 100
-				mov edx, 0
-				div ebx
-
-				// ja com 0.11*b em eax, adicionamos 0.59*g e 0.3*red obtendo a media
-				add eax, green
-				add eax, red
-				mov media, eax
-
-				// Calculos nos canais de cores para o tom sepia
-				mov eax, media
-				add eax, 40
-				cmp eax, 255
-				ja SET_R_FF
-				mov red, eax
-				jmp GREEN
-			SET_R_FF :
-			mov eax, 255
-				mov red, eax
-			GREEN :
-			mov eax, media
-				add eax, 20
-				cmp eax, 255
-				ja SET_G_FF
-				mov green, eax
-				jmp BLUE
-			SET_G_FF :
-			mov eax, 255
-				mov green, eax
-			BLUE :
-			mov eax, media
-				sub eax, 20
-				cmp eax, 255
-				ja SET_0_B
-				mov blue, eax
-				jmp FIM
-			SET_0_B :
-			mov eax, 0
-				mov blue, eax
-			FIM :
-		}
-		imagem[j] = red;
-		imagem[j + 1] = green;
-		imagem[j + 2] = blue;
-	}
-	*/
 	//Calculos para o tom sepia paralelo (SSE2)
 	
-	for (j = 0; j < tamanhoImg - 1; j += 3)
-	{
-		// media = 0.3*r + 0.59*g + 0.11*b
-		// r = media + 40
-		// g = media + 20
-		// b = media - 20
-
-		//printf("%d\n", j);
-		temp = &imagem[j];
-		temp2 = soma;
-		temp3 = subtracao;
-		//temp4 = fatores;
-		red = (int)imagem[j + 3];
-		/*printf("%x\n", temp);
-		printf("Antes:\n");
-		printf("%d\n", imagem[j]);
-		printf("%d\n", imagem[j + 1]);
-		printf("%d\n", imagem[j + 2]);
-		printf("%d\n", imagem[j + 3]);
-		printf("%d\n", imagem[j + 4]);
-		printf("%d\n", imagem[j + 5]);
-		printf("%d\n", imagem[j + 6]);
-		printf("%d\n", imagem[j + 7]);
-		printf("%d\n", imagem[j + 8]);
-		*/
-
-		__asm
-		{
-			mov eax, temp
-			lea ebx, a
-			movd xmm0, [eax]
-			movups xmm1, [a]
-			pxor xmm2, xmm2 //zera mm2
-			//punpcklbw xmm0, xmm2
-			pmullw xmm0, xmm1
-			lea eax, soma
-			lea ebx, subtracao
-			movd xmm3, eax
-			movd xmm4, ebx
-			paddw xmm0, xmm2
-			psubw xmm0, xmm2
-
-			//packuswb xmm0, xmm2
-
-
-			mov eax, temp
-			movd [eax],xmm0
-			
-
-
-
-
-		}
-		imagem[j + 3] = red;
-		/*printf("Depois:\n");
-		printf("%d\n", imagem[j]);
-		printf("%d\n", imagem[j + 1]);
-		printf("%d\n", imagem[j + 2]);
-		printf("%d\n", imagem[j + 3]);
-		printf("%d\n", imagem[j + 4]);
-		printf("%d\n", imagem[j + 5]);
-		printf("%d\n", imagem[j + 6]);
-		printf("%d\n", imagem[j + 7]);
-		printf("%d\n\n", imagem[j + 8]);
-		*/
-
-	}
 	
-	end = clock();
+
+		for (j = 0; j < tamanhoImg - 1; j += 3)
+		{
+			// media = 0.3*r + 0.59*g + 0.11*b
+			// r = media + 40
+			// g = media + 20
+			// b = media - 20
+
+			
+			temp = &imagem[j];
+			//red = (int)imagem[j];
+			///green = (int)imagem[j + 1];
+			//blue = (int)imagem[j + 2];
+			//redt = (int)imagem[j + 3];
+
+
+			b.x = (float)imagem[j];
+			b.y = (float)imagem[j + 1];
+			b.z = (float)imagem[j + 2];
+			b.w = (float)imagem[j + 3];
+
+
+			__asm
+			{
+				lea eax, b
+				lea ebx, a
+				movdqu xmm0, [eax]
+				movdqu xmm1, [ebx]
+				mulps xmm0, xmm1
+				movdqu [eax], xmm0
+			}
+
+			imagem[j] = (unsigned char)b.x;
+			imagem[j + 1] = (unsigned char)b.y;
+			imagem[j + 2] = (unsigned char)b.z;
+			imagem[j + 3] = (unsigned char)b.w;
+
+
+			imagem[j] += imagem[j + 1] + imagem[j + 2];
+			imagem[j + 1] = imagem[j];
+			imagem[j + 2] = imagem[j];
+
+			__asm
+			{
+				mov eax, temp
+				movd mm0, [eax]
+				movd mm3, soma
+				movd mm4, subtracao
+				paddusb mm0, mm3
+				psubusb mm0, mm4
+
+
+				mov eax, temp
+				movd[eax], mm0
+
+				emms
+			}
+
+
+			
+		}
+
+		end = clock();
+
 
 	// Escrever novo arquivo
 	if ((file = fopen(outputFileDir, "wb")) != NULL)
@@ -219,8 +132,10 @@ int main(int argc, char** argv)
 
 	fclose(file);
 	free(imagem);
+
 	cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-	fprintf(stderr, "tempo = %f segundos\n", cpu_time_used);
+	fprintf(stderr, "tempo SSE = %f segundos\n", cpu_time_used);
+	
 
 	return 0;
 }
@@ -231,3 +146,4 @@ Referencias:
 [1]: http://neilkemp.us/src/sse_tutorial/sse_tutorial.html
 
 */
+
